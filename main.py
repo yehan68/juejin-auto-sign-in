3dfe7642d1f070d2463999bf42c8521546da424f
yehan <zhangtong.mr@aliyun.com>
2022-12-03 10:34:39 +0800

    update: 增加沾喜气获取幸运值

--- main.py
+++ main.py
@@ -9,19 +9,38 @@ config = {
   'cookie': os.environ.get('COOKIE'),
 }
 
-response = requests.post(
+dipResponse = requests.post(
+  url = 'https://api.juejin.cn/growth_api/v1/lottery_lucky/dip_lucky?aid=' + config['aid'] + '&uuid=' + config['uuid'] + '&spider=0',
+  headers = {
+    'cookie': config['cookie']
+  }
+)
+
+if dipResponse.status_code == 200:
+  dipResult = dipResponse.json()
+  if dipResult['err_no']:
+    print("签到失败: " + dipResult['err_msg'])
+    print('本次沾喜气获得' + str(dipResult['data']['dip_value']) + '幸运值')
+    print('当前累计幸运值为' + str(dipResult['data']['total_value']))
+  else:
+    print('本次沾喜气获得' + str(dipResult['data']['dip_value']) + '幸运值')
+    print('当前累计幸运值为' + str(dipResult['data']['total_value']))
+
+checkResponse = requests.post(
   url = 'https://api.juejin.cn/growth_api/v1/check_in?aid=' + config['aid'] + '&uuid=' + config['uuid'] + '&_signature=' + config['_signature'],
   headers = {
     'cookie': config['cookie']
   }
 )
 
-if response.status_code == 200:
-  result = response.json()
-  if result['err_no']:
-    print("签到失败: " + result['err_msg'])
+if checkResponse.status_code == 200:
+  checkResult = checkResponse.json()
+  if checkResult['err_no']:
+    print("签到失败: " + checkResult['err_msg'])
+    print('本次签到领取' + str(checkResult['data']['incr_point']) + '钻石')
+    print('当前累计为' + str(checkResult['data']['sum_point']) + '钻石')
   else:
-    print('本次签到领取' + str(result['data']['incr_point']) + '钻石')
-    print('当前累计为' + str(result['data']['sum_point']) + '钻石')
+    print('本次签到领取' + str(checkResult['data']['incr_point']) + '钻石')
+    print('当前累计为' + str(checkResult['data']['sum_point']) + '钻石')
 
 exit()
