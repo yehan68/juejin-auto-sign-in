d6942d01da54b620a09ef2163780d326558de5d7
yehan <zhangtong.mr@aliyun.com>
2024-02-29 22:46:26 +0800

    fix: 注释沾喜气接口

--- main.py
+++ main.py
@@ -9,22 +9,23 @@ config = {
   'cookie': os.environ.get('COOKIE'),
 }
 
-dipResponse = requests.post(
-  url = 'https://api.juejin.cn/growth_api/v1/lottery_lucky/dip_lucky?aid=' + config['aid'] + '&uuid=' + config['uuid'] + '&spider=0',
-  headers = {
-    'cookie': config['cookie']
-  }
-)
+# 沾喜气接口已经下线
+# dipResponse = requests.post(
+#   url = 'https://api.juejin.cn/growth_api/v1/lottery_lucky/dip_lucky?aid=' + config['aid'] + '&uuid=' + config['uuid'] + '&spider=0',
+#   headers = {
+#     'cookie': config['cookie']
+#   }
+# )
 
-if dipResponse.status_code == 200:
-  dipResult = dipResponse.json()
-  if dipResult['err_no']:
-    print("签到失败: " + dipResult['err_msg'])
-    print('本次沾喜气获得' + str(dipResult['data']['dip_value']) + '幸运值')
-    print('当前累计幸运值为' + str(dipResult['data']['total_value']))
-  else:
-    print('本次沾喜气获得' + str(dipResult['data']['dip_value']) + '幸运值')
-    print('当前累计幸运值为' + str(dipResult['data']['total_value']))
+# if dipResponse.status_code == 200:
+#   dipResult = dipResponse.json()
+#   if dipResult['err_no']:
+#     print("签到失败: " + dipResult['err_msg'])
+#     print('本次沾喜气获得' + str(dipResult['data']['dip_value']) + '幸运值')
+#     print('当前累计幸运值为' + str(dipResult['data']['total_value']))
+#   else:
+#     print('本次沾喜气获得' + str(dipResult['data']['dip_value']) + '幸运值')
+#     print('当前累计幸运值为' + str(dipResult['data']['total_value']))
 
 checkResponse = requests.post(
   url = 'https://api.juejin.cn/growth_api/v1/check_in?aid=' + config['aid'] + '&uuid=' + config['uuid'] + '&_signature=' + config['_signature'],
